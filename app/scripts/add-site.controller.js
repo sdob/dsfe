a12796406d91a8e74278b411d86bc3c54fbc6d0e
sdob <sdob@cs.ucc.ie>
2015-12-29 18:43:26 +0000

    basic add site skeleton and layout

--- app/scripts/add-site.controller.js
+++ app/scripts/add-site.controller.js
@@ -1,13 +1,19 @@
 (function () {
   'use strict';
-  function AddSiteController() {
+  function AddSiteController(mapSettings) {
+    const vm = this;
     activate();
 
     function activate() {
       console.log('AddSiteController.activate()');
+      vm.map = mapSettings.get();
+      vm.submit = submit;
+    }
+    function submit() {
+      console.log('submitted!');
     }
   }
 
-  AddSiteController.$inject = [];
+  AddSiteController.$inject = ['mapSettings'];
   angular.module('divesites').controller('AddSiteController', AddSiteController);
 })();
