092d26952636af3969a2189925f88cfaca615268
sdob <sdob@cs.ucc.ie>
2015-12-31 17:29:18 +0000

    report coverage

--- karma.conf.js
+++ karma.conf.js
@@ -48,8 +48,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/**/*.js': ['babel'],
-      'test/**/*.js': ['babel']
+      'app/**/*.js': ['babel', 'coverage',],
+      'test/**/*.js': ['babel'],
     },
 
     babelPreprocessor: {
@@ -63,8 +63,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/',
+    },
 
     // web server port
     port: 9876,
