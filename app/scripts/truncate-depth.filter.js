b72c58f079e7e43ba3c56708070daec83caf4cc2
sdob <sdob@cs.ucc.ie>
2015-12-31 17:21:34 +0000

    remove extraneous filter

--- app/scripts/truncate-depth.filter.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  function truncateDepth() {
-    return (depth) => parseInt(depth * 100) / 100;
-  }
-
-  angular.module('divesites').filter('truncateDepth', truncateDepth);
-})();
