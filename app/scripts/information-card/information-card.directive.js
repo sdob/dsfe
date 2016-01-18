7115aa0beb6b10ab22601d4c91724eb40231cb0a
sdob <sdob@cs.ucc.ie>
2016-01-18 15:45:24 +0000

    responsive: info card header collapses/uncollapses card

--- app/scripts/information-card/information-card.directive.js
+++ app/scripts/information-card/information-card.directive.js
@@ -7,11 +7,15 @@
       controller: 'InformationCardController',
       controllerAs: 'icvm',
       link: (scope, element, attrs, controller, transcludeFn) => {
-        //console.log(element.find('.information-card__collapse-chevron'));
+        // TODO: can probably remove the on-click from the chevron element,
+        // since it just provides a nice visual hint of the parent element's
+        // behaviour
         element.find('.information-card__collapse-chevron')
         .click(function (e) {
-          //console.log($(this));
-          //$(this).toggleClass('opened');
+          element.find('.information-card').toggleClass('opened');
+        });
+        element.find('.information-card__title')
+        .click(function (e) {
           element.find('.information-card').toggleClass('opened');
         });
       },
