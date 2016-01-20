b2d004d477f78a6d7b83608b05aa9bcdaae11343
sdob <sdob@cs.ucc.ie>
2016-01-20 17:35:02 +0000

    add-image behaviour for slipway information card

--- app/scripts/information-card/slipway-information-card.controller.js
+++ app/scripts/information-card/slipway-information-card.controller.js
@@ -12,7 +12,35 @@
 
       /* Wire up functions */
       vm.isAuthenticated = $auth.isAuthenticated;
+      vm.toggleSectionVisibility = toggleSectionVisibility;
       vm.userIsOwner = informationCardService.userIsOwner(vm.site);
+
+      vm.sectionVisibilities = {
+        defaultSection: true,
+        uploadImageForm: false,
+      };
+    }
+
+    function toggleSectionVisibility(section) {
+      console.log('toggle');
+      // If we've been passed garbage, just return
+      if (!vm.sectionVisibilities.hasOwnProperty(section)) return;
+
+      // Otherwise, render the selected section visible
+      if (vm.sectionVisibilities[section]) {
+        // If the section is currently visible, hide it, then
+        // show the dive list (the default view)
+        vm.sectionVisibilities[section] = false;
+        vm.sectionVisibilities.defaultSection = true;
+      } else {
+        // If the section is currently invisible, set
+        // all other section visibilities to false and the upload image
+        // form's visibility to true
+        Object.keys(vm.sectionVisibilities).forEach((k) => {
+          vm.sectionVisibilities[k] = false;
+        });
+        vm.sectionVisibilities[section] = true;
+      }
     }
   }
 
