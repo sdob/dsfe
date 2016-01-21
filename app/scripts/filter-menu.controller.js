c04f50b9878d3f83b11c91a174a995162c210262
sdob <sdob@cs.ucc.ie>
2016-01-21 18:34:05 +0000

    insert TODO on filter-menu controller

--- app/scripts/filter-menu.controller.js
+++ app/scripts/filter-menu.controller.js
@@ -16,21 +16,26 @@
     }
 
     function handleSearchSelect(item) {
-      console.log(`handling select for '${item.name}'`);
-      console.log($scope.mc);
+      // When the user selects a divesite from the search results, re-centre
+      // the map, then emulate a click on the map marker
       $scope.mc.map.center = {
         latitude: item.latitude,
         longitude: item.longitude,
       };
 
       // Emulate a marker click here
-      console.log($scope.mc.mapMarkers);
       const marker = $scope.mc.mapMarkers.filter((m) => m.id === item.id)[0];
       console.log(marker);
       $scope.mc.markerEvents.click(marker, undefined, item);
     }
 
     function updatePreferences() {
+      // TODO: this causes several events to be broadcast every time a single
+      // change is made. This needs to be updated so that a single object is
+      // built, and the preferences service receives and handles it and
+      // broadcasts a single event. (The reason this *needs* doing is that the
+      // MapController has to iterate over every map marker whenever it receives
+      // a filter preference change event.
       Object.keys(vm.preferences).forEach((k) => {
         filterPreferences.set(k, vm.preferences[k]);
       });
