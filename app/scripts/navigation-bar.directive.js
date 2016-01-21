47373c6eb370846fc0d85f43ac9c9758202df5e8
sdob <sdob@cs.ucc.ie>
2016-01-21 13:36:17 +0000

    navbar is absolutely positioned; dropdown is in front of filter menu and add button; dropdown collapses when a nav element is clicked

--- app/scripts/navigation-bar.directive.js
+++ app/scripts/navigation-bar.directive.js
@@ -6,6 +6,13 @@
       controller: 'NavigationBarController',
       controllerAs: 'nbvm',
       link: () => {
+        // Collapse navigation bar dropdown when one of its items is clicked; see
+        // http://stackoverflow.com/questions/21203111/bootstrap-3-collapsed-menu-doesnt-close-on-click
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+          if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
+            $(this).collapse('hide');
+          }
+        });
       },
     };
   }
