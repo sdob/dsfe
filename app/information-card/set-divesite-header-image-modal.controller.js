84e8775dcd884048eac5c40cdc025b4d372f02ec
sdob <sdob@cs.ucc.ie>
2016-02-01 16:16:28 +0000

    Actually go through with divesite header image upload

--- app/information-card/set-divesite-header-image-modal.controller.js
+++ app/information-card/set-divesite-header-image-modal.controller.js
@@ -1,5 +1,5 @@
 (function() {
-  function SetDivesiteHeaderImageModalController($scope, $timeout, Upload, site, IMG_API_URL) {
+  function SetDivesiteHeaderImageModalController($scope, $timeout, $uibModalInstance, Upload, site, IMG_API_URL) {
     const vm = this;
     activate();
 
@@ -13,12 +13,25 @@
     function setDivesiteHeaderImage(file) {
       console.log(`I should upload and close the modal`);
       console.log(file);
+
       // show the user that we're uploading
       vm.isUploading = true;
-      $timeout(() => {
-        console.log('finished uploading');
-        vm.isUploading = false;
-      }, 1000)
+
+      console.log('uploading to');
+      console.log(`{IMG_API_URL}/divesites/${site.id}/header`);
+
+      // Upload the file
+      file.upload = Upload.upload({
+        data: { image: file },
+        url: `${IMG_API_URL}/divesites/${site.id}/header`,
+      }).then(() => {
+        // On success, update UI and close us on out
+        $timeout(() => {
+          console.log('finished uploading');
+          vm.isUploading = false;
+          $uibModalInstance.close();
+        }, 200);
+      })
       .then((response) => {
       })
       .catch((err) => {
@@ -30,6 +43,7 @@
   SetDivesiteHeaderImageModalController.$inject = [
     '$scope',
     '$timeout',
+    '$uibModalInstance',
     'Upload',
     'site',
     'IMG_API_URL',
