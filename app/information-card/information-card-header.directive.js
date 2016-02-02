d22b24d2816e3c61682f47b36b0158d1b08386a1
sdob <sdob@cs.ucc.ie>
2016-02-02 13:54:48 +0000

    info card header now has all the logic it needs to handle close and dismiss

--- app/information-card/information-card-header.directive.js
+++ app/information-card/information-card-header.directive.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  function informationCardHeader() {
+  function informationCardHeader($document, informationCardService) {
     return {
       controller: 'InformationCardHeaderController',
       controllerAs: 'vm',
@@ -13,12 +13,36 @@
     };
 
     function link(scope, element, attrs, controller) {
-      console.log('informationCardHeader.link()');
-      console.log(scope);
+      const keydownListener = informationCardService.escapeKeydownListener(removeSelf);
+      // Add event listeners
+      $document.on('keydown', keydownListener);
+
+      element.find('.information-card__dismiss-button').on('click', removeSelf);
+      element.find('.information-card__title').on('click', toggleOpened);
+
+      element.on('$destroy', () => {
+        // Remove event listeners
+        $document.off('keydown', keydownListener);
+        element.find('.information-card__dismiss-button').off('click', removeSelf);
+        element.find('.information-card__title').off('click', toggleOpened);
+      });
+
+      function removeSelf() {
+        // Ask parent scope to remove us
+        console.log('sending a please kill me event');
+        scope.$emit('please-kill-me');
+      }
+
+      function toggleOpened() {
+        $document.find('.information-card').toggleClass('opened');
+      }
     }
+
   }
 
   informationCardHeader.$inject = [
+    '$document',
+    'informationCardService',
   ];
   angular.module('divesites.informationCard').directive('informationCardHeader', informationCardHeader);
 })();
