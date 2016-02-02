b101be49da1259c59356441072f486f99e336d85
sdob <sdob@cs.ucc.ie>
2016-02-02 13:56:14 +0000

    info card just manually destroys scope

--- app/information-card/information-card.directive.js
+++ app/information-card/information-card.directive.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function informationCard($document, $location, collapseBehaviour, informationCardService) {
+  function informationCard() {
     return {
       templateUrl: 'information-card/information-card.html',
       controller: 'InformationCardController',
@@ -9,63 +9,18 @@
       link,
     };
 
-    function link(scope, element, attrs, controller, transcludeFn) {
-      const keydownListener = informationCardService.escapeKeydownListener(removeSelf);
-      const toggleOpened = informationCardService.toggleOpened(element);
-
-      // XXX: This seems hacky, but the alternative *appears* to be inserting
-      // 'onload' markup into the templates
-      let dismissButtonHasLoaded = false;
-      let titleHasLoaded = false;
-
-      // Register listeners
-      $document.on('keydown', keydownListener);
-
-      // When the ng-includes have finished running, assign event listeners
-      scope.$on('$includeContentLoaded', (evt) => {
-
-        // When the title element has loaded, give it open/close behaviour
-        if (!titleHasLoaded && element.find('.information-card__title').length) {
-          titleHasLoaded = true;
-          element.find('.information-card__title').on('click', toggleOpened);
-        }
-
-        // When the dismiss button has loaded, give it dismiss behaviour
-        if (!dismissButtonHasLoaded && element.find('.information-card__dismiss-button').length) {
-          dismissButtonHasLoaded = true;
-          element.find('.information-card__dismiss-button').on('click', removeSelf);
-        }
-      });
+    function link(scope, element, attrs, controller) {
 
       // Clean up
       element.on('$destroy', () => {
-        // Remove click listener
-        element.find('.information-card__title').off('click', toggleOpened);
-
-        // Remove dismiss listener
-        element.find('.information-card__dismiss-button').off('click', removeSelf);
-
-        // Remove ESC key
-        $document.off('keydown', keydownListener);
-
         // Because we created this scope manually, we need to destroy it
         // manually too
         scope.$destroy();
       });
-
-      function removeSelf() {
-        // politely ask parent scope to remove me
-        scope.$emit('please-kill-me', element);
-        console.log('sending a please-kill-me event');
-      }
     }
   }
 
   informationCard.$inject = [
-    '$document',
-    '$location',
-    'collapseBehaviour',
-    'informationCardService',
   ];
   angular.module('divesites.informationCard').directive('informationCard', informationCard);
 })();
