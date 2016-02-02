37c7324de5f5f827ac9f11d99e659d467b40b4dc
sdob <sdob@cs.ucc.ie>
2016-02-02 14:01:12 +0000

    thinner info card directives all round

--- app/information-card/slipway-information-card.directive.js
+++ app/information-card/slipway-information-card.directive.js
@@ -1,51 +1,26 @@
 (function() {
   'use strict';
 
-  function slipwayInformationCard($document, $location, informationCardService) {
+  function slipwayInformationCard() {
     return {
       templateUrl: 'information-card/slipway-information-card.html',
       controller: 'SlipwayInformationCardController',
       controllerAs: 'icvm',
-      link: (scope, element, attrs, controller, transcludeFn) => {
-        const keydownListener = informationCardService.escapeKeydownListener(removeSelf);
-        const toggleOpened = informationCardService.toggleOpened(element);
-        let dismissButtonHasLoaded = false;
-        let titleHasLoaded = false;
-
-        $document.on('keydown', keydownListener);
-
-        scope.$on('$includeContentLoaded', (evt) => {
-          if (!titleHasLoaded && element.find('.information-card__title').length) {
-            titleHasLoaded = true;
-            element.find('.information-card__title').on('click', toggleOpened);
-          }
-
-          if (!dismissButtonHasLoaded && element.find('.information-card__dismiss-button')) {
-            dismissButtonHasLoaded = true;
-            element.find('.information-card__dismiss-button').on('click', removeSelf);
-          }
-        });
-
-        // Clean up
-        element.on('$destroy', () => {
-          element.find('.information-card__title').off('click', toggleOpened);
-          element.find('.information-card__dismiss-button').off('click', removeSelf);
-          $document.off('keydown', keydownListener);
-          // Manually destroy scope
-          scope.$destroy();
-        });
-
-        function removeSelf() {
-          scope.$emit('please-kill-me', element);
-        }
-      },
+      link,
     };
+
+    function link(scope, element, attrs, controller) {
+
+      // Clean up
+      element.on('$destroy', () => {
+        // Because we created this scope manually, we need to destroy it
+        // manually too
+        scope.$destroy();
+      });
+    }
   }
 
   slipwayInformationCard.$inject = [
-    '$document',
-    '$location',
-    'informationCardService',
   ];
   angular.module('divesites.informationCard').directive('slipwayInformationCard', slipwayInformationCard);
 })();
