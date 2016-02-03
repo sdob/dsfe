f9149aad42e685aae15cfcf6c95cc15292fe9820
sdob <sdob@cs.ucc.ie>
2016-02-03 15:19:45 +0000

    pass message into modal-mask as attribute

--- app/widgets/modal-mask.directive.js
+++ app/widgets/modal-mask.directive.js
@@ -2,8 +2,19 @@
   'use strict';
   function modalMask() {
     return {
+      controller: function() {
+        console.log('modalMask controller');
+      },
+
+      link,
+
       templateUrl: 'widgets/modal-mask.html',
     };
+
+    function link(scope, element, attrs) {
+      console.log('modal mask!');
+      scope.message = attrs.message;
+    }
   }
 
   angular.module('divesites.widgets').directive('modalMask', modalMask);
