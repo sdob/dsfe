cb8ef7af379f1d6b43e50c15c30ff4e2b1cfacf4
sdob <sdob@cs.ucc.ie>
2016-02-09 13:30:38 +0000

    submit functionality for upload-image modal

--- app/information-card/upload-image-modal.controller.js
+++ app/information-card/upload-image-modal.controller.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function UploadImageModalController($uibModalInstance, site) {
+  function UploadImageModalController($uibModalInstance, Upload, dsimg, site) {
     const vm = this;
     activate();
 
@@ -10,12 +10,29 @@
       vm.submit = submit;
     }
 
-    function submit() {
+    function submit(file) {
+      vm.isUploading = true;
+      const siteID = vm.site.id;
+      console.log(`uploading for site ${siteID}`);
+      file.upload = Upload.upload({
+        data: { image: file },
+        url: `${dsimg.IMG_API_URL}/divesites/${siteID}`,
+      })
+      .then(() => {
+        $uibModalInstance.close('image-uploaded');
+      })
+      .catch((err) => {
+        vm.isUploading = false;
+        // TODO: show error message
+      })
+      ;
     }
   }
 
   UploadImageModalController.$inject = [
     '$uibModalInstance',
+    'Upload',
+    'dsimg',
     'site',
   ];
   angular.module('divesites.informationCard').controller('UploadImageModalController', UploadImageModalController);
