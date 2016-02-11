4379e194cc96d3255f1158edbe0e51efe8cd3aa1
sdob <sdob@cs.ucc.ie>
2016-02-11 16:12:28 +0000

    add slipway comments to dscomments service

--- app/apis/dscomments.service.js
+++ app/apis/dscomments.service.js
@@ -3,6 +3,9 @@
 
   function dscommentsService($auth, $http, API_URL) {
     return {
+
+      /* Divesite comments */
+
       getDivesiteComments: (id) => {
         return $http.get(`${API_URL}/divesites/${id}/comments/`);
       },
@@ -17,6 +20,24 @@
 
       deleteDivesiteComment: (id) => {
         return $http.delete(`${API_URL}/comments/divesites/${id}/`);
+      },
+
+      /* Slipway comments */
+
+      getSlipwayComments: (id) => {
+        return $http.get(`${API_URL}/slipways/${id}/comments/`);
+      },
+
+      postSlipwayComment: (data) => {
+        return $http.post(`${API_URL}/comments/slipways/`, data);
+      },
+
+      updateSlipwayComment: (id, data) => {
+        return $http.patch(`${API_URL}/comments/slipways/${id}/`, data);
+      },
+
+      deleteSlipwayComment: (id) => {
+        return $http.delete(`${API_URL}/comments/slipways/${id}/`);
       },
     };
   }
