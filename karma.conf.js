5a456012e9b3bf633a8f4c5f7633ec910cd3d7e4
sdob <sdob@cs.ucc.ie>
2016-02-14 10:32:29 +0000

    deps: add Bluebird

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'node_modules/babel-polyfill/dist/polyfill.js',
       // bower:js
       'bower_components/jquery/dist/jquery.js',
+      'bower_components/bluebird/js/browser/bluebird.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
