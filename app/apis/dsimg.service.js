c5bf141b0f19d79f131d08df0616ba2aa8f55cc9
sdob <sdob@cs.ucc.ie>
2016-02-26 16:02:35 +0000

    Add content_type dictionary and delete route for images

--- app/apis/dsimg.service.js
+++ app/apis/dsimg.service.js
@@ -2,8 +2,20 @@
   'use strict';
 
   function dsimgService($http, API_URL) {
+
+    /* These are potentially subject to change if there's a
+     * major rewrite of the API.
+     */
+    const CONTENT_TYPES = {
+      8: 'divesite',
+      10: 'compressor',
+      11: 'slipway',
+    };
+
     return {
       API_URL,
+      CONTENT_TYPES,
+      deleteSiteImage,
       deleteUserProfileImage,
       getSiteHeaderImage,
       getSiteImages,
@@ -17,6 +29,10 @@
 
     function getSiteHeaderImage(site) {
       return $http.get(`${API_URL}/${site.type}s/${site.id}/header/`);
+    }
+
+    function deleteSiteImage(site, id) {
+      return $http.delete(`${API_URL}/${site.type}s/${site.id}/images/${id}/`);
     }
 
     function getSiteImages(site) {
