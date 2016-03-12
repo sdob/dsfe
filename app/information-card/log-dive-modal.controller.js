e3e0a3829a19de11c82f8596c222f3ade2dec9ab
sdob <sdob@cs.ucc.ie>
2016-03-12 13:41:19 +0000

    Monitor gas mix settings and send them as a JSON object on submit

--- app/information-card/log-dive-modal.controller.js
+++ app/information-card/log-dive-modal.controller.js
@@ -14,14 +14,33 @@
       vm.options = {
         showMeridian: uiPreferencesService.get().clock === '12hr',
       };
+      vm.setNitrox = setNitrox;
       vm.site = site;
       vm.submit = submit;
 
+      // Set defaults for this dive
       const dt = logDiveService.defaultDateAndTime();
       vm.dive = {
         date: dt.date,
+        gasMix: 'air',
         site: vm.site,
       };
+    }
+
+    function ensureNitroxO2ContentIsDefined() {
+      console.log('ensuring nitroxO2 content is defined');
+      vm.dive.nitroxO2 = vm.dive.nitroxO2 || 21;
+    }
+
+    function formatGasMix() {
+      const obj = {
+        mix: vm.dive.gasMix,
+      };
+      // If the mix is Nitrox, add the O2 content
+      if (vm.dive.gasMix === 'nitrox') {
+        obj.o2 = vm.dive.nitroxO2;
+      }
+      return obj;
     }
 
     function formatRequest(dive) {
@@ -36,12 +55,29 @@
         divesite: vm.site.id,
         air_temperature: dive.airTemperature,
         water_temperature: dive.waterTemperature,
+        // Tank pressure
+        pressure_in: dive.pressureIn,
+        pressure_out: dive.pressureOut,
+        // Gas mix
+        gas_mix: formatGasMix(),
       };
       return request;
     }
 
     function openDatepicker() {
       vm.datepicker.opened = true;
+    }
+
+    function setNitrox(isNitrox) {
+      // Set the gas mix to Nitrox when focusing the O2 input,
+      // even if the user hasn't used the radio button
+      if (isNitrox) {
+        vm.dive.gasMix = 'nitrox';
+        vm.dive.nitroxO2 = vm.dive.nitroxO2 || 21;
+        // vm.ensureNitroxO2ContentIsDefined();
+      } else {
+        delete vm.dive.nitroxO2;
+      }
     }
 
     function submit() {
@@ -56,6 +92,8 @@
       const request = formatRequest(vm.dive);
       console.log('sending request');
       console.log(request);
+      vm.isSubmitting = false;
+      /*
       dsapi.postDive(request)
       .then((response) => {
         $uibModalInstance.close('logged');
@@ -75,6 +113,7 @@
           console.error(err);
         }
       });
+      */
     }
   }
 
