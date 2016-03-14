129e1e4ad66ed95dea12f8e341de5dec49c26b6b
sdob <sdob@cs.ucc.ie>
2016-03-14 15:03:32 +0000

    Extract wind/weather constants to service

--- app/information-card/log-dive-modal.controller.js
+++ app/information-card/log-dive-modal.controller.js
@@ -1,31 +1,8 @@
 (function() {
   'use strict';
-  function LogDiveModalController($scope, $timeout, $uibModalInstance, dsapi, logDiveService, modalService, site, uiPreferencesService) {
+  function LogDiveModalController($scope, $timeout, $uibModalInstance, conditionsLayoutService, dsapi, logDiveService, modalService, site, uiPreferencesService) {
     const vm = this;
-
-    const weathers = [
-      { type: 'clear', wiClass: 'wi-day-sunny', text: 'Clear' },
-      { type: 'clouds', wiClass: 'wi-cloud', text: 'Clouds' },
-      { type: 'rain', wiClass: 'wi-rain', text: 'Rain' },
-      { type: 'fog', wiClass: 'wi-fog', text: 'Fog' },
-      { type: 'snow', wiClass: 'wi-snow', text: 'Snow' },
-    ];
-
-    const winds = [
-      { value: 0, description: 'Calm' },
-      { value: 1, description: 'Light air' },
-      { value: 2, description: 'Light breeze' },
-      { value: 3, description: 'Gentle breeze' },
-      { value: 4, description: 'Moderate breeze' },
-      { value: 5, description: 'Fresh breeze' },
-      { value: 6, description: 'Strong breeze' },
-      { value: 7, description: 'High wind' },
-      { value: 8, description: 'Gale' },
-      { value: 9, description: 'Strong gale' },
-      { value: 10, description: 'Storm' },
-      { value: 11, description: 'Violent storm' },
-      { value: 12, description: 'Hurricane' },
-    ];
+    const { weathers, winds } = conditionsLayoutService;
 
     activate();
 
@@ -182,6 +159,7 @@
     '$scope',
     '$timeout',
     '$uibModalInstance',
+    'conditionsLayoutService',
     'dsapi',
     'logDiveService',
     'modalService',
