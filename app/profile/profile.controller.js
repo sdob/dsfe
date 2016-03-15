bc9cb427799df4a3939837e686560a989156c3e2
sdob <sdob@cs.ucc.ie>
2016-03-15 13:32:07 +0000

    Check for content_type_model instead of divesiteID, etc.

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -42,16 +42,23 @@
             let apiCall;
             let id;
             // FIXME: ugly hack because dsimg doesn't return a site type
-            if (i.hasOwnProperty('divesiteID')) {
+            if (i.content_type_model === 'divesite') {
               apiCall = dsapi.getDivesite;
-              id = i.divesiteID;
-            } else if (i.hasOwnProperty('slipwayID')) {
+              id = i.object_id;
+            } else if (i.content_type_model === 'slipway') {
               apiCall = dsapi.getSlipway;
-              id = i.slipwayID;
-            } else if (i.hasOwnProperty('compressorID')) {
+              id = i.object_id;
+            } else if (i.content_type_model === 'compressor') {
               apiCall = dsapi.getCompressor;
-              id = i.compressorID;
+              id = i.object_id;
             }
+
+            if (!apiCall) {
+              console.error(`I was expecting apiCall to be defined, but it isn't`);
+              console.error(i);
+              return; // uh-oh, get out
+            }
+
             //dsapi.getDivesite(i.divesiteID)
             apiCall(id)
             .then((response) => {
