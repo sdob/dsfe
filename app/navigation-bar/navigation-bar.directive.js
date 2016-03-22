53e1726dffc7de9c4cea54f9f1d6fc93114fe9b6
sdob <sdob@cs.ucc.ie>
2016-03-22 17:17:38 +0000

    Fix code style on navbar directive

--- app/navigation-bar/navigation-bar.directive.js
+++ app/navigation-bar/navigation-bar.directive.js
@@ -2,19 +2,21 @@
   'use strict';
   function NavigationBar() {
     return {
-      templateUrl: 'navigation-bar/navigation-bar.template.html',
       controller: 'NavigationBarController',
       controllerAs: 'nbvm',
-      link: () => {
-        // Collapse navigation bar dropdown when one of its items is clicked; see
-        // http://stackoverflow.com/questions/21203111/bootstrap-3-collapsed-menu-doesnt-close-on-click
-        $(document).on('click', '.navbar-collapse.in', function(e) {
-          if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
-            $(this).collapse('hide');
-          }
-        });
-      },
+      link,
+      templateUrl: 'navigation-bar/navigation-bar.template.html',
     };
+
+    function link() {
+      // Collapse navigation bar dropdown when one of its items is clicked; see
+      // http://stackoverflow.com/questions/21203111/bootstrap-3-collapsed-menu-doesnt-close-on-click
+      $(document).on('click', '.navbar-collapse.in', function(e) {
+        if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
+          $(this).collapse('hide');
+        }
+      });
+    }
   }
 
   angular.module('divesites.navigationBar').directive('navigationBar', NavigationBar);
