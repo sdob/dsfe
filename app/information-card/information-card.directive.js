65c8da4d96bb85d269c835830b526e79915d074a
sdob <sdob@cs.ucc.ie>
2016-03-23 10:09:38 +0000

    Clean up comments in info card directive

--- app/information-card/information-card.directive.js
+++ app/information-card/information-card.directive.js
@@ -13,15 +13,13 @@
       // If the site is a divesite, then activate the dive tab;
       // if not, then activate the comments tab. This behaves a bit
       // weirdly because at the time the directive is linked, ng-if
-      // hasn't yet resolved, so ng-if DOM elements aren't in the DOm
+      // hasn't yet resolved, so ng-if DOM elements aren't in the DOM
       // and consequently jQuery can't access the tab by ID; that's
       // why we use ng-show instead.
       let selector;
       if (scope.vm.site.type === 'divesite') {
-        console.log('there is a dive list');
         selector = '#js-dive-list-tab';
       } else {
-        console.log('no dive list');
         selector = '#js-comment-list-tab';
       }
 
@@ -29,8 +27,8 @@
 
       // Clean up
       element.on('$destroy', () => {
-        // Because we created this scope manually, we need to destroy it
-        // manually too
+        // Because we created this scope manually in MapController, we need to
+        // destroy it manually too
         scope.$destroy();
       });
     }
