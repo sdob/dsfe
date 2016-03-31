ab72bcf23d9e8123847950a14a34e1a597109401
sdob <sdob@cs.ucc.ie>
2016-03-31 11:56:24 +0100

    Add injectable constants for third-party scripts

--- app/app.vendor-constants.js
+++ app/app.vendor-constants.js
@@ -1,3 +1,9 @@
-angular.module('divesites')
+(function() {
+  'use strict';
+  // Following the Papa style guide, define injectable constants for
+  // third-party JS
+  angular.module('divesites')
   .constant('d3', d3)
+  .constant('haversine', haversine)
   .constant('moment', moment);
+})();
