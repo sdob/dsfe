eabd5e8f65b5f427b1126d78d00d1e0d181e783d
sdob <sdob@cs.ucc.ie>
2016-03-31 16:40:53 +0100

    Remove dead code from info card header controller

--- app/information-card/header/information-card-header.controller.js
+++ app/information-card/header/information-card-header.controller.js
@@ -5,15 +5,6 @@
     activate();
 
     function activate() {
-      console.log('InformationCardHeadercontroller.activate()');
-      console.log($scope);
-      //$scope.site = $scope.site;
-      // {
-        //const apiCall = getApiCallByType($scope.site.type);
-        // Get the divesite header image (if it exists)
-        //informationCardService.getDivesiteHeaderImage($scope.site.id)
-        //apiCall($scope.site.id)
-      // }
       getAndApplySiteHeaderImage();
 
       $scope.$on('header-image-changed', () => {
@@ -57,20 +48,6 @@
           // vm.headerImageUrl = null;
         }
       });
-      /*
-      informationCardService.getSiteHeaderImage($scope.site)
-      .then((imageUrl) => {
-        console.log('something came back from dsimg');
-        // If there's an image, dsimg will return 200 and a non-null object
-        if (imageUrl) {
-          console.log(imageUrl);
-          vm.headerImageUrl = imageUrl;
-          vm.backgroundStyle = {
-            background: `blue url(${vm.headerImageUrl}) center / cover`,
-          };
-        }
-      });
-      */
     }
   }
 
