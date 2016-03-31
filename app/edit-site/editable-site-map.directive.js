adee225bb2fb763d704e75effc879f2d3e4da04b
sdob <sdob@cs.ucc.ie>
2016-03-31 11:58:07 +0100

    Clean up editable site map directive

--- app/edit-site/editable-site-map.directive.js
+++ app/edit-site/editable-site-map.directive.js
@@ -5,10 +5,14 @@
       restrict: 'E',
       templateUrl: 'edit-site/editable-site-map.template.html',
       link: (scope, elem, attrs, ctrl) => {
+        // Wait for the Google Maps API to load, then place a marker object
+        // in front of it to indicate the site's location
         uiGmapGoogleMapApi
         .then((maps) => {
           const mapContainer = elem.find('.angular-google-map-container')[0];
-          $('<div/>').addClass(`editable-site-map__centre-marker ${scope.vm.siteTypeString}`)
+
+          $('<div/>')
+          .addClass(`editable-site-map__centre-marker ${scope.vm.siteTypeString}`)
           .appendTo(mapContainer);
         });
       },
