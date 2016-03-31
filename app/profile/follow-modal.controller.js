f0df8c3560c498a39c5cab34417c41cbd549cfb1
sdob <sdob@cs.ucc.ie>
2016-03-31 15:32:30 +0100

    Retrieve follow suggestions if we're looking for them

--- app/profile/follow-modal.controller.js
+++ app/profile/follow-modal.controller.js
@@ -8,27 +8,32 @@
     function activate() {
       vm.direction = direction; // 'followers' or 'follows'?
       vm.dismiss = dismiss;
+      vm.getSuggestions = getSuggestions;
       vm.follow = follow;
       vm.isAuthenticated = $auth.isAuthenticated;
       vm.unfollow = unfollow;
       vm.user = user; // The user whose list we're viewing
       vm.users = users; // The members of the list we're viewing
 
-      // Get profile thumbnails for each user in the list
-      vm.users.forEach((user) => {
-        dsimg.getUserProfileImage(user.id)
-        .then((response) => {
-          if (response && response.data && response.data.public_id) {
-            user.profileImageUrl = $.cloudinary.url(response.data.public_id, {
-              height: 60,
-              width: 60,
-              crop: 'fill',
-              gravity: 'face',
-            });
-          }
-        });
-      });
+      // If we're going to show suggestions, we need to show them first
+      if (direction === 'suggestions') {
+        console.log('finding suggestions');
+        vm.getSuggestions()
+        .then((users) => {
+          console.log('suggestions returned!');
+          console.log(users);
+          vm.users = users;
+        })
+        .then(getProfileThumbnails)
+        .then(checkFollowStatuses);
+      } else {
+        getProfileThumbnails();
+        checkFollowStatuses();
+      }
 
+    }
+
+    function checkFollowStatuses() {
       // If the viewing user is authenticated, then allow them to follow/unfollow
       // the members of the list
       if (vm.isAuthenticated()) {
@@ -54,6 +59,28 @@
         // Update UI
         user.viewingUserIsFollowing = true;
       });
+    }
+
+    function getProfileThumbnails() {
+      // Get profile thumbnails for each user in the list
+      vm.users.forEach((user) => {
+        dsimg.getUserProfileImage(user.id)
+        .then((response) => {
+          if (response && response.data && response.data.public_id) {
+            user.profileImageUrl = $.cloudinary.url(response.data.public_id, {
+              height: 60,
+              width: 60,
+              crop: 'fill',
+              gravity: 'face',
+            });
+          }
+        });
+      });
+    }
+
+    function getSuggestions(user) {
+      console.log('getting suggestions');
+      return followService.getSuggestions();
     }
 
     function unfollow(user) {
