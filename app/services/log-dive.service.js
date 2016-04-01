e94aa1c039e5f80afef417f0ec573341a4376a8d
sdob <sdob@cs.ucc.ie>
2016-04-01 12:14:03 +0100

    Add log-dive service back

---
+++ app/services/log-dive.service.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+  function logDiveService() {
+    return {
+      combineDateAndTime: (date, time) => {
+        const year = moment(date).year();
+        const month = moment(date).month();
+        const day = moment(date).date();
+        const hour = moment(time).hour();
+        const minute = moment(time).minute();
+        const combined = moment([year, month, day, hour, minute]);
+        return combined;
+      },
+
+      defaultDateAndTime: () => {
+        return {
+          date: new Date(),
+          time: new Date(),
+        };
+      },
+    };
+  }
+
+  logDiveService.$inject = [];
+  angular.module('divesites').factory('logDiveService', logDiveService);
+})();
