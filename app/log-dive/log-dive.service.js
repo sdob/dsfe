dc45472085e41464e25d1ac60387e96f63a5e284
sdob <sdob@cs.ucc.ie>
2016-04-06 16:55:22 +0100

    Refactor log dive service to conform to style guide

--- app/log-dive/log-dive.service.js
+++ app/log-dive/log-dive.service.js
@@ -2,25 +2,41 @@
   'use strict';
   function logDiveService($uibModal) {
     return {
-      combineDateAndTime: (date, time) => {
-        const year = moment(date).year();
-        const month = moment(date).month();
-        const day = moment(date).date();
-        const hour = moment(time).hour();
-        const minute = moment(time).minute();
-        const combined = moment([year, month, day, hour, minute]);
-        return combined;
-      },
-
-      defaultDateAndTime: () => {
-        return {
-          date: new Date(),
-          time: new Date(),
-        };
-      },
-
+      combineDateAndTime,
+      defaultDateAndTime,
+      summonConfirmDiveDeletionModal,
       summonLogDiveModal,
     };
+
+    function combineDateAndTime(date, time) {
+      const year = moment(date).year();
+      const month = moment(date).month();
+      const day = moment(date).date();
+      const hour = moment(time).hour();
+      const minute = moment(time).minute();
+      const combined = moment([year, month, day, hour, minute]);
+      return combined;
+    }
+
+    function defaultDateAndTime() {
+      return {
+        date: new Date(),
+        time: new Date(),
+      };
+    }
+
+    function summonConfirmDiveDeletionModal(dive) {
+      return $uibModal.open({
+        controller: 'ConfirmDiveDeletionModalController',
+        controllerAs: 'vm',
+        resolve: {
+          dive: () => dive,
+        },
+        size: 'sm',
+        templateUrl: 'log-dive/confirm-dive-deletion-modal.template.html',
+        windowClass: 'modal-center',
+      });
+    }
 
     function summonLogDiveModal(dive, site) {
       return $uibModal.open({
