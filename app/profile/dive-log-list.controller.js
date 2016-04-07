de4e06940efdce6838cdc3656009e037b0c8ef27
sdob <sdob@cs.ucc.ie>
2016-04-07 14:23:44 +0100

    Emit an event when dives are edited or deleted from the dive log view

--- app/profile/dive-log-list.controller.js
+++ app/profile/dive-log-list.controller.js
@@ -8,30 +8,36 @@
     function activate() {
       console.log('DiveLogListController.activate()');
       vm.summonConfirmDiveDeletionModal = summonConfirmDiveDeletionModal;
-      vm.summonDiveLogModal = summonDiveLogModal;
-      console.log($scope);
+      vm.summonLogDiveModal = summonLogDiveModal;
+    }
+
+    /* Return a function that takes a reason, compares it to the expected reason,
+     * and emits an event to parent scope if the two match
+     */
+    function emitEventIfSuccessful(expectedReason, event) {
+      return (reason) => {
+        if (reason === expectedReason) {
+          $scope.$emit(event);
+        }
+      };
     }
 
     function summonConfirmDiveDeletionModal(dive) {
       console.log('deleting');
       const instance = logDiveService.summonConfirmDiveDeletionModal(dive);
-      instance.result.then((reason) => {
-        if (reason === 'deleted') {
-          console.log('dive sucessfully deleted');
-          // TODO: Other components within the profile should update their
-          // knowledge of the dive log when it changes
-          $scope.$emit('dive-log-updated');
-          dsapi.getUserDives($scope.user.id)
-          .then((response) => {
-            $timeout(() => {
-              $scope.dives = response.data;
-            });
-          });
-        }
-      });
+      // We might need to emit an event when the modal closes
+      instance.result.then(emitEventIfSuccessful('deleted', 'dive-log-updated'));
     }
 
-    function summonDiveLogModal() {
+    function summonLogDiveModal(dive) {
+      // We need to retrieve the divesite details first
+      dsapi.getDivesite(dive.divesite.id)
+      .then((response) => {
+        const site = response.data;
+        const instance = logDiveService.summonLogDiveModal(dive, site);
+        // We might need to emit an event when the modal closes
+        instance.result.then(emitEventIfSuccessful('logged', 'dive-log-updated'));
+      });
     }
   }
 
