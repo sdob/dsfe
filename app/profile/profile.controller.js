598e77ca2ce2d7ed6ae23112ca9e79064f628fdc
sdob <sdob@cs.ucc.ie>
2016-04-11 16:03:50 +0100

    Move immediately-bindable values out of activate block and send images-added request in parallel with main profile request

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -2,38 +2,45 @@
   'use strict';
   function ProfileController($routeParams, $rootScope, $scope, $timeout, dsactivity, dsapi, dsimg, informationCardService, profileService, userSettingsService) {
     const vm = this;
+
+    // Get the user's ID and put it into $scope
+    const userId = $routeParams.userId;
+    $scope.userId = userId;
+    vm.userId = userId;
+    vm.user = {
+      id: userId,
+    };
+
+    // Run the activate block
     activate();
 
     function activate() {
       console.log('ProfileController.activate()');
 
-      // Get the user's ID and put it into $scope
-      const userId = $routeParams.userId;
-      vm.userId = userId;
-      $scope.userId = userId;
-      vm.user = {
-        id: userId,
-      };
+      // Get this user's profile information from the API server
+      dsapi.getUser(userId)
+      .then((response) => {
+        // Format the incoming response
+        vm.user = profileService.formatResponseData(response.data);
+        // Format the places-added  list
+        vm.user.placesAdded = profileService.formatUserProfilePlacesAdded(vm.user);
+        // Broadcast an event to child controllers letting them know that
+        // the information has arrived
+        $scope.$broadcast('user-loaded', vm.user);
+      })
 
+      // Retrieve images this user has uploaded
+      dsimg.getUserImages(vm.user.id)
+      .then((response) => {
+        vm.user.imagesAdded = profileService.formatUserProfileImagesAdded(response);
+      });
+
+      // Listen for events emitted by the dive log controller
       $scope.$on('dive-log-updated', (e) => {
         dsapi.getUserDives(userId)
         .then((response) => {
           vm.user.dives = response.data;
         });
-      });
-
-      // Get this user's profile information from the API server
-      dsapi.getUser(userId)
-      .then((response) => {
-        vm.user = profileService.formatResponseData(response.data);
-        vm.user.placesAdded = profileService.formatUserProfilePlacesAdded(vm.user);
-        $scope.$broadcast('user-loaded', vm.user);
-
-        // Retrieve images this user's uploaded
-        return dsimg.getUserImages(vm.user.id);
-      })
-      .then((response) => {
-        vm.user.imagesAdded = profileService.formatUserProfileImagesAdded(response);
       });
     }
   }
