e4371d8cfb73f9dd7d38e902ced5d7c48e8c43be
sdob <sdob@cs.ucc.ie>
2016-04-16 10:58:01 +0100

    Clean up code to make sequential operations more obvious

--- app/caching/user-thumbnail-cache.service.js
+++ app/caching/user-thumbnail-cache.service.js
@@ -22,34 +22,45 @@
      * otherwise return the cached value
      */
     function get(id) {
+      // If the cache doesn't have this ID, make the request
       if (cache[id] === undefined) {
         console.log(`userThumbnailCache has nothing for ${id}`);
-        // return something
         console.log('retrieving from API');
-        return dsimg.getUserProfileImage(id)
-        .then((response) => {
-          if (response.data && response.data.public_id) {
-            const thumbnailURL = $.cloudinary.url(response.data.public_id, {
-              height: 60,
-              width: 60,
-              crop: 'fill',
-              gravity: 'face',
-            });
-            // Add to cache
-            set(id, thumbnailURL);
-            return thumbnailURL;
-          }
 
-          return Promise.resolve(undefined);
-        });
+        return dsimg.getUserProfileImage(id)
+        .then(formatResponse)
+        .then((value) => set(id, value));
+      } else {
+        // If we have something stored, wrap it in a Promise and return it
+        return Promise.resolve(cache[id]);
       }
 
-      return Promise.resolve(cache[id]);
+      function formatResponse(response) {
+        // Acquire the URL for a 60x60 Cloudinary image
+        if (response.data && response.data.public_id) {
+          const thumbnailURL = $.cloudinary.url(response.data.public_id, {
+            height: 60,
+            width: 60,
+            crop: 'fill',
+            gravity: 'face',
+          });
+          return thumbnailURL;
+        }
+
+        return undefined;
+      }
+
+      function retrieveFromApi(id) {
+        // Request the user profile image from our API
+        return dsimg.getUserProfileImage(id);
+      }
+
     }
 
     /* Insert or overwrite the cached value */
     function set(id, value) {
       cache[id] = value;
+      return value;
     }
   }
 
