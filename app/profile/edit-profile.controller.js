7d7527cc31f7c74543c0b4187543b745f67221a1
sdob <sdob@cs.ucc.ie>
2016-04-18 16:28:13 +0100

    Return to the user's own profile after saving/cancelling a profile edit

--- app/profile/edit-profile.controller.js
+++ app/profile/edit-profile.controller.js
@@ -18,6 +18,8 @@
     }
 
     function cancel() {
+      // Check whether the form has been changed and summon a confirmation
+      // modal
       if ($scope.editForm.$dirty) {
         const instance = $uibModal.open({
           templateUrl: 'profile/cancel-edit-profile-modal.template.html',
@@ -26,11 +28,11 @@
         });
         instance.result.then((reason) => {
           if (reason === 'perform-cancel') {
-            $location.path('/me');
+            $location.path(`/users/${vm.user.id}`);
           }
         });
       } else {
-        $location.path('/me');
+        $location.path(`/users/${vm.user.id}`);
       }
     }
 
@@ -41,7 +43,7 @@
       .then((response) => {
         vm.isSaving = false;
         console.log(response.data);
-        $location.path('/me');
+        $location.path(`/users/${vm.user.id}`);
       })
       .catch((err) => {
         console.error(err);
