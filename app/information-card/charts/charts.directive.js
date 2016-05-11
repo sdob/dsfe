e05641edccac2c87f40ecbfa0ffdf1196301ea75
sdob <sdob@cs.ucc.ie>
2016-05-11 14:05:38 +0100

    Extract link to function def

--- app/information-card/charts/charts.directive.js
+++ app/information-card/charts/charts.directive.js
@@ -4,57 +4,59 @@
     return {
       controller: 'InformationCardChartsController',
       controllerAs: 'vm',
-      // scope: true,
+      link,
       scope: {
         site: '=',
       },
       templateUrl: 'information-card/charts/charts.template.html',
-      link: (scope, element, attrs, controller) => {
-        $('#js-information-card__depth-chart-toggle, #js-information-card__duration-chart-toggle')
-        .click(collapseBehaviour.toggleChevron);
-
-        element.on('$destroy', () => {
-          scope.$destroy();
-        });
-
-        scope.$on('destroy', () => {
-        });
-
-        // Rebuild histograms when told to
-        scope.$on('refresh-statistics', (evt, site) => {
-          console.log('receiving refresh-statistics event');
-          scope.vm.site = site;
-          buildCharts(site, element);
-        });
-      },
     };
+
+    function link(scope, element, attrs, controller) {
+      $('#js-information-card__depth-chart-toggle, #js-information-card__duration-chart-toggle')
+      .click(collapseBehaviour.toggleChevron);
+
+      element.on('$destroy', () => {
+        scope.$destroy();
+      });
+
+      scope.$on('destroy', () => {
+      });
+
+      // Rebuild histograms when told to
+      scope.$on('refresh-statistics', (evt, site) => {
+        console.log('receiving refresh-statistics event');
+        console.log(site);
+        scope.vm.site = site;
+        buildCharts(site, element);
+      });
+    }
 
     function buildCharts(site, element) {
       const avg = (l) => l.reduce((x, y) => x + y) / l.length;
-          // Update site in scope to refresh average depth and duration values
-          //scope.vm.site = site;
+      // Update site in scope to refresh average depth and duration values
+      //scope.vm.site = site;
 
-          /* Refresh histograms */
+      /* Refresh histograms */
 
-          // Remove existing histograms
-          element.find('#information-card-depth-histogram-container').empty();
-          element.find('#information-card-duration-histogram-container').empty();
-          if (site.dives.length) {
-            // Construct new histograms
-            const depths = site.dives.map((d) => Number(d.depth));
-            const durations = site.dives.map(d => moment.duration(d.duration).minutes());
-            // The end of the x-scale for depth and duration is either the maximum depth
-            // or large enough to put the mean in the middle
-            const maxDepth = Math.max(Math.max(...depths), avg(depths) * 2);
-            const maxDuration = Math.max(Math.max(...durations), avg(durations) * 2);
-            //console.log(Math.max(depths) * 2);
-            const depthHistogram = informationCardChartsService.createHistogram('depth', depths, 20, 512, 178, 0, maxDepth);
-            const durationHistogram = informationCardChartsService.createHistogram('duration', durations, 20, 512, 178, 0, maxDuration);
-            element.find('#information-card-depth-histogram-container').append(depthHistogram);
+      // Remove existing histograms
+      element.find('#information-card-depth-histogram-container').empty();
+      element.find('#information-card-duration-histogram-container').empty();
+      if (site.dives.length) {
+        // Construct new histograms
+        const depths = site.dives.map((d) => Number(d.depth));
+        const durations = site.dives.map(d => moment.duration(d.duration).minutes());
+        // The end of the x-scale for depth and duration is either the maximum depth
+        // or large enough to put the mean in the middle
+        const maxDepth = Math.max(Math.max(...depths), avg(depths) * 2);
+        const maxDuration = Math.max(Math.max(...durations), avg(durations) * 2);
+        //console.log(Math.max(depths) * 2);
+        const depthHistogram = informationCardChartsService.createHistogram('depth', depths, 20, 512, 178, 0, maxDepth);
+        const durationHistogram = informationCardChartsService.createHistogram('duration', durations, 20, 512, 178, 0, maxDuration);
+        element.find('#information-card-depth-histogram-container').append(depthHistogram);
 
-            element.find('#information-card-duration-histogram-container').append(durationHistogram);
-          }
-        }
+        element.find('#information-card-duration-histogram-container').append(durationHistogram);
+      }
+    }
   }
 
   informationCardCharts.$inject = [
