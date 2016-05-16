3c0570d36be09c78b9136a897981d9d4521aede2
sdob <sdob@cs.ucc.ie>
2016-05-16 16:13:27 +0100

    Extract link function

--- app/edit-site/edit-divesite.directive.js
+++ app/edit-site/edit-divesite.directive.js
@@ -5,16 +5,18 @@
     return {
       templateUrl: 'edit-site/edit-divesite.template.html',
       restrict: 'E',
-      link: (scope, elem, attrs, ctrl) => {
-        console.log('editDivesite.link()');
-        $('#add-site__boat-entry').on('click', () => {
-          scope.vm.checkAtLeastOneEntryIsSelected();
-        });
-        $('#add-site__shore-entry').on('click', () => {
-          scope.vm.checkAtLeastOneEntryIsSelected();
-        });
-      },
+      link,
     };
+
+    function link(scope, elem, attrs, ctrl) {
+      console.log('editDivesite.link()');
+      $('#add-site__boat-entry').on('click', () => {
+        scope.vm.checkAtLeastOneEntryIsSelected();
+      });
+      $('#add-site__shore-entry').on('click', () => {
+        scope.vm.checkAtLeastOneEntryIsSelected();
+      });
+    }
   }
 
   editDivesite.$inject = [];
