15c5025e8410aba15db2fa6d49490d6d04cdc0df
sdob <sdob@cs.ucc.ie>
2016-05-23 17:06:16 +0100

    Use public_id sent in event to update thumbnail
    When the user updates their profile image, $rootScope will broadcast an
    event that includes the new Cloudinary public_id, which we can use here
    to request a thumbnail-sized version without making a new request to the
    API.

--- app/navigation-bar/navigation-bar.controller.js
+++ app/navigation-bar/navigation-bar.controller.js
@@ -12,14 +12,17 @@
     profileService
   ) {
     const vm = this;
+    // Set bindable values
+    vm.isAuthenticated = $auth.isAuthenticated;
+    vm.signOut = signOut;
+    vm.summonLoginModal = summonLoginModal;
+
     activate();
 
+    /*
+     * Code to be executed on controller activation
+     */
     function activate() {
-      // Set bindable values
-      vm.isAuthenticated = $auth.isAuthenticated;
-      vm.signOut = signOut;
-      vm.summonLoginModal = summonLoginModal;
-
       // Try and download a thumbnail image if we have a user ID
       if (localStorageService.get('user')) {
         const userID = localStorageService.get('user');
@@ -30,35 +33,54 @@
         });
       }
 
-      $scope.$on('profile-image-changed', () => {
-        if (localStorageService.get('user')) {
-          const userID = localStorageService.get('user');
-          retrieveAndFormatProfileThumbnailImage(userID)
-          .then((url) => {
-            vm.userProfileThumbnailImage = url;
-          });
-        }
+      // When the user changes their profile image, update the thumbnail
+      // in the nav bar
+      $scope.$on('profile-image-changed', (evt, public_id) => {
+        $timeout(() => {
+          const url = formatAsThumbnail(public_id);
+          vm.userProfileThumbnailImage = formatAsThumbnail(public_id);
+        });
       });
     }
 
+    /*
+     * Given a public_id, convert it to a Cloudinary URL for an 18x18 version
+     */
+    function formatAsThumbnail(public_id) {
+      return $.cloudinary.url(public_id, {
+        height: 18,
+        width: 18,
+        crop: 'fill',
+        gravity: 'face',
+      });
+    }
+
+    /*
+     * Take the API response, extract the public_id, and return its formatted
+     * thumbnail URL
+     */
     function formatDsimgResponse(response) {
       if (response.data && response.data.public_id) {
-        return $.cloudinary.url(response.data.public_id, {
-          height: 18,
-          width: 18,
-          crop: 'fill',
-          gravity: 'face',
-        });
+        return formatAsThumbnail(response.data.public_id);
       }
 
       return undefined;
     }
 
+    /*
+     * Make the API request and pass the response to a formatting function
+     */
     function retrieveAndFormatProfileThumbnailImage(id) {
       return dsimg.getUserProfileImage(id)
       .then(formatDsimgResponse);
     }
 
+    /*
+     * Do signing-out clear-up
+     * TODO: some of this is shared behaviour (e.g., clearing localStorage)
+     * and should go into a shared service somewhere so that we can call it
+     * from anywhere, not just this controller
+     */
     function signOut() {
       localStorageService.remove('user');
       profileService.clear(); // remove own profile data from profileService
