81a9b08d96dac7a1cc9a0b014d4893957a15b6da
sdob <sdob@cs.ucc.ie>
2016-05-23 17:03:07 +0100

    Return new Cloudinary public_id on modal close
    With this commit, the modal returns an object including the new
    Cloudinary public_id when upload is successful. This allows us to send
    the new public_id as an event from $rootScope so that other scopes don't
    need to make their own calls to dsimg.

--- app/profile/upload-profile-image-modal.controller.js
+++ app/profile/upload-profile-image-modal.controller.js
@@ -2,14 +2,22 @@
   'use strict';
   function ProfileImageUploadController($scope, $uibModalInstance, Upload, dsimg, user) {
     const vm = this;
+
+    vm.cancelUpload = cancelUpload;
+    vm.submit = submit;
     vm.user = user;
+
     activate();
 
+    /*
+     * Activation-time code
+     */
     function activate() {
-      vm.cancelUpload = cancelUpload;
-      vm.submit = submit;
     }
 
+    /*
+     * Cancel uploading a file (not implemented yet in the UI)
+     */
     function cancelUpload() {
       // Close the modal instance
       // TODO: check whether a file has been previewed or is in mid-upload
@@ -17,6 +25,10 @@
       $uibModalInstance.close();
     }
 
+    /*
+     * Upload the file, and on success close the modal, sending back the CLoudinary
+     * public_id for the new file
+     */
     function submit(file) {
       // Disable the upload button
       vm.isSaving = true;
@@ -27,12 +39,13 @@
         url: `${dsimg.API_URL}/users/${user.id}/profile_image/`,
       })
       .then((response) => {
-        // Return us to the profile view, closing the modal instance
-        // on the way
+        // Update the UI flag
         vm.isSaving = false;
-
-        // Close the modal
-        $uibModalInstance.close('uploaded');
+        // Close the modal, sending the public_id as part of the reason
+        $uibModalInstance.close({
+          reason: 'uploaded',
+          public_id: response.data.public_id,
+        });
       });
     }
   }
